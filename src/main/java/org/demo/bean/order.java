9c599581e537685ca49f7ca6c1e04a193d6aa6d3
Kristings <1449303720@qq.com>
2023-03-04 15:28:35 +0800

    第2次提交

--- src/main/java/org/demo/bean/order.java
+++ src/main/java/org/demo/bean/order.java
@@ -1,5 +1,10 @@
 package org.demo.bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.demo.enums.OrderStatus;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -7,5 +12,9 @@ package org.demo.bean;
  * @Date: 2023/03/04/11:55
  * @Description:
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class order {
+    OrderStatus orderStatus;
 }
